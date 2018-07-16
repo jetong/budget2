8e1aec8cbc866cf2bd20be3553757d28bb45f95a
jetong <jose.e.tong@gmail.com>
2018-07-16 00:59:44 +0000

    Removed processForm() and other code that no longer applies in version2. Added categories array to hold category objects. Category names and colors are assigned to each object.

--- public/javascript/script.js
+++ public/javascript/script.js
@@ -1,7 +1,7 @@
 var canvas;
 var context;
-var colors = [];
-var expenses = [];
+let categories = [];
+
 
 let today = new Date().toISOString().substr(0, 10);
 document.getElementById("date").value = today;
@@ -89,7 +89,7 @@ $("#btn-submit").click( () => {
 
 // Execute on page load/refresh
 function init() {
-	console.log("initializing");
+
 	// Get canvas graphics context
 	canvas = document.getElementById("canvas");
 	context = canvas.getContext("2d");
@@ -110,126 +110,75 @@ function init() {
 
 // Execute on 'submit expenses' button click
 function main() {
-// Process form and store values in cache
-processForm();
-// Render visual data
-draw();
-// Display balance
-displayBalance();
+	// Render visual data
+	draw();
 }
 
 
 // Execute on 'start over' button click
 function startOver() {
-// Clear session stored values
-sessionStorage.clear();
-expenses = [];
-
-// Clear input fields
-//var fields = document.getElementsByTagName("input");
-//for(var i = 0; i < fields.length; i++) {
-//    fields[i].value = "";
-//}
-//document.getElementById("balance").style.background = "";
-
-// Clear canvas
-context.clearRect(0, 0, canvas.width, canvas.height);
-
-// Clear key
-keyDiv = document.getElementById("key");
-while (keyDiv.firstChild) {
-    keyDiv.removeChild(keyDiv.firstChild);
-}
-
-randomizeColors();
-}
-
-
-function processForm() {
-// Initialize map for expense categories
-var prevExpenses = {};
-var inputElements = document.getElementsByClassName("expenses");
-var keys = [];
-for(var i = 0; i < inputElements.length; i++) {
-    keys[i] = inputElements[i].id;
-// If current key is undefined, define it with value 0
-if(!prevExpenses[keys[i]]) {
-  prevExpenses[keys[i]] = 0;
-}
-}
-
-// Get previous key/values from sessionStorage and store into map
-// Note: Doesn't execute on first run as there are no stored key/values initially and the length is 0.
-for(var i = 0; i < Object.keys(sessionStorage).length; i++) {
-    var key = Object.keys(sessionStorage)[i];
-    var value = sessionStorage.getItem(key);
-    prevExpenses[key] = parseInt(value);
-}
-
-// Get current values submitted by user and accumulate into prevExpenses map
-for(var i = 0; i < inputElements.length; i++) {
-    if(inputElements[i].value) {
-      prevExpenses[inputElements[i].id] += parseInt(inputElements[i].value);
-      expenses[i] = prevExpenses[inputElements[i].id];
-} else { // In case field input is empty/undefined
-  prevExpenses[inputElements[i].id] += 0;
-}
-}
-
-// Set storage to the latest cumulative total
-for(var key in prevExpenses) {
-    sessionStorage.setItem(key, parseInt(prevExpenses[key]));
-}
+	expenses = [];
+	
+	// Clear canvas
+	context.clearRect(0, 0, canvas.width, canvas.height);
+	
+	// Clear key
+	keyDiv = document.getElementById("key");
+	while (keyDiv.firstChild) {
+	    keyDiv.removeChild(keyDiv.firstChild);
+	}
+	
+	randomizeColors();
 }
 
 
 function draw() {
-    drawPieChart(canvas.width/2, canvas.height/2, canvas.width*.4);
-    drawKey();
+	drawPieChart(canvas.width/2, canvas.height/2, canvas.width*.4);
+	drawKey();
 }
 
 
 // Draw pie chart one section (category) at a time
 function drawPieChart(x, y, radius) {
-// Current pie section's ratio to the entire pie
-var ratio;
+	// Current pie section's ratio to the entire pie
+	var ratio;
+	
+	// Sum up expenses to calculate individual ratios
+	var totalExpenses = 0;
+	for(var i = 0; i < expenses.length; i++) {
+	  totalExpenses += expenses[i];
+	}
+	
+	// Starting and ending angles of each pie section
+	var startingAngle = 0;
+	var endingAngle = 0;
+	
+	for(var i = 0; i < expenses.length; i++) {
+	  context.beginPath();
 
-// Sum up expenses to calculate individual ratios
-var totalExpenses = 0;
-for(var i = 0; i < expenses.length; i++) {
-    totalExpenses += expenses[i];
-}
-
-// Starting and ending angles of each pie section
-var startingAngle = 0;
-var endingAngle = 0;
-
-for(var i = 0; i < expenses.length; i++) {
-    context.beginPath();
-
-    context.fillStyle = colors[i];
-
-    ratio = expenses[i]/totalExpenses;
-    endingAngle = startingAngle + Math.PI*2*ratio;
-
-    context.arc(x, y, radius, startingAngle, endingAngle);
-    context.lineTo(x, y);
-    context.fill();
-    context.stroke();
-    context.closePath();
-    startingAngle = endingAngle;
-}
+		context.fillStyle = colors[i];
+	
+		ratio = expenses[i]/totalExpenses;
+		endingAngle = startingAngle + Math.PI*2*ratio;
+		
+		context.arc(x, y, radius, startingAngle, endingAngle);
+		context.lineTo(x, y);
+		context.fill();
+		context.stroke();
+		context.closePath();
+		startingAngle = endingAngle;
+	}
 }
 
 
 // Dynamically create key for pie chart
 function drawKey() {
-    keyDiv = document.getElementById("key");
+	keyDiv = document.getElementById("key");
 
-// Remove previous key items
-while (keyDiv.firstChild) {
-    keyDiv.removeChild(keyDiv.firstChild);
-}
+	// Remove previous key items
+	while (keyDiv.firstChild) {
+		keyDiv.removeChild(keyDiv.firstChild);
+	}
 
 // Append new key items
 var inputElements = document.getElementsByClassName("expenses");
@@ -265,36 +214,21 @@ keyDiv.appendChild(item);
 }
 
 
-// Display balance for current session
-function displayBalance() {
-    var income = document.getElementById("income").value || 0;
-
-    var totalExpenses = 0;
-    for(var i = 0; i < expenses.length; i++) {
-        totalExpenses += expenses[i];
-    }
-
-    var balance = income - totalExpenses;
-    document.getElementById("balance").value = balance;
-    if(balance < 0) {
-        document.getElementById("balance").style.background = "#ffdddd";
-    } else {
-        document.getElementById("balance").style.background = "#ddffdd";
-    }
-}
-
 
 // Helper functions
 
-// Fill colors[] with random colors for each expense category
+$("#category option").each(function() {
+	categories.push({category: $(this).val()}); 
+});
+
+// Assign random colors for each category
 function randomizeColors() {
-    var categories = document.getElementsByClassName("expenses");
-    for(var i = 0; i < categories.length; i++) {
-        var red = Math.trunc(Math.random()*255);
-        var green = Math.trunc(Math.random()*255);
-        var blue = Math.trunc(Math.random()*255);
-        colors[i] = "rgb(" + red + "," + green + "," + blue + ")";
-    }
+		for(var i = 0; i < categories.length; i++) {
+			var red = Math.trunc(Math.random()*255);
+			var green = Math.trunc(Math.random()*255);
+			var blue = Math.trunc(Math.random()*255);
+			categories[i].color = "rgb(" + red + "," + green + "," + blue + ")";
+		}
 }
 
 
@@ -311,3 +245,5 @@ function resizeCanvas() {
     canvas.height = document.getElementById("canvas-wrapper").clientHeight;
 }
 
+
+console.log(categories);
